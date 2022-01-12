bf9397df3150f8a4eee49e9068083529ef376890
Opilane TTHK <opilane@tthk.ee>
2022-01-12 11:18:55 +0200

    sss

---
+++ OdeToFood/Controllers/CuisineController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OdeToFood.filter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Controllers
+{
+    [Log]
+    public class CuisineController : Controller
+    {
+
+        public IActionResult Search(string name = "french")
+        {
+            throw new Exception("Something terrible happend! :O");
+
+            return Content("Esimene:"+name);
+        }
+    }
+}
---
+++ OdeToFoodTests/Controller/HomeControllerTests.cs
@@ -0,0 +1,32 @@
+﻿//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.Extensions.Logging;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using OdeToFood.Controllers;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace OdeToFoodTests.Controller
+//{
+//    [TestClass]
+//    public class HomeControllerTests
+//    {
+//        [TestMethod]
+//        public void About()
+//        {
+//            ////Arrange
+//            //using var logFactory = LoggerFactory.Create(builder => builder.AddConsole());
+//            //var logger = logFactory.CreateLogger<HomeController>();
+//            //HomeController controller = new HomeController(logger);
+
+//            ////act
+//            //ViewResult result = controller.About() as ViewResult;
+//            ////assert
+//            //Assert.IsNotNull(result.Model);
+
+
+//        }
+//    }
+//}
